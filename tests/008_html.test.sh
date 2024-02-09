0268f5d43b80872deeb69a1290bc0bb11e4a3572
Dimitri Gilbert <dimitri.gilbert@gmail.com>
2024-02-09 19:14:05 +0100

    unit test more html-form

--- tests/008_html.test.sh
+++ tests/008_html.test.sh
@@ -4,7 +4,7 @@ source "./lib/bashunit_ext"
 pabin=./parseArger
 ex=("$pabin" generate)
 exp=("$pabin" html-form)
-spos=("my-arg" "'An argument'")
+spos=("my-arg" "'An argument'" --one-of val --one-of val1 --one-of val2)
 sopt=("my-opt" "'An opt'" -r -s o --alias opt-pt --default-value "'aValue'")
 sflg=("my-flag" "'A flag'" -s f --no-name nope --alias flg --no-alias noflg --on)
 snst=("my-nst" "'A nested opt'")
@@ -85,6 +85,21 @@ function test_html_input_class() {
   rm "$tfile";
 }
 
+function test_html_input_container_class() {
+  local tfile="${tftmp_file}_html_input_container_class";
+  local ex_=("${ex[@]}" --output "$tfile" --opt "${sopt[*]}" --flag "${sflg[*]}" --nested "${snst[*]}" --pos "${spos[*]}")
+  local pex_=("${exp[@]}" "$tfile" --input-container-class "my-input-class")
+  
+  assert_exit_code "0" "$("${ex_[@]}")";
+  assert_is_file "$tfile";
+
+  local pexout="$("${pex_[@]}")";
+  assert_exit_code "0" "$?";
+  assert_contains "my-input-class" "$pexout";
+
+  rm "$tfile";
+}
+
 function test_html_label_class() {
   local tfile="${tftmp_file}_html_label_class";
   local ex_=("${ex[@]}" --output "$tfile" --opt "${sopt[*]}" --flag "${sflg[*]}" --nested "${snst[*]}" --pos "${spos[*]}")
@@ -96,6 +111,96 @@ function test_html_label_class() {
   local pexout="$("${pex_[@]}")";
   assert_exit_code "0" "$?";
   assert_contains "my-label-class" "$pexout";
+
+  rm "$tfile";
+}
+
+function test_html_select_class() {
+  local tfile="${tftmp_file}_html_select_class";
+  local ex_=("${ex[@]}" --output "$tfile" --opt "${sopt[*]}" --flag "${sflg[*]}" --nested "${snst[*]}" --pos "${spos[*]}")
+  local pex_=("${exp[@]}" "$tfile" --select-class "my-select-class")
+  
+  assert_exit_code "0" "$("${ex_[@]}")";
+  assert_is_file "$tfile";
+
+  local pexout="$("${pex_[@]}")";
+  assert_exit_code "0" "$?";
+  assert_contains "my-select-class" "$pexout";
+
+  rm "$tfile";
+}
+
+function test_html_checkbox_label_class() {
+  local tfile="${tftmp_file}_html_checkbox_label_class";
+  local ex_=("${ex[@]}" --output "$tfile" --opt "${sopt[*]}" --flag "${sflg[*]}" --nested "${snst[*]}" --pos "${spos[*]}")
+  local pex_=("${exp[@]}" "$tfile" --checkbox-label-class "my-checkbox-label-class")
+  
+  assert_exit_code "0" "$("${ex_[@]}")";
+  assert_is_file "$tfile";
+
+  local pexout="$("${pex_[@]}")";
+  assert_exit_code "0" "$?";
+  assert_contains "my-checkbox-label-class" "$pexout";
+
+  rm "$tfile";
+}
+
+function test_html_checkbox_class() {
+  local tfile="${tftmp_file}_html_checkbox_class";
+  local ex_=("${ex[@]}" --output "$tfile" --opt "${sopt[*]}" --flag "${sflg[*]}" --nested "${snst[*]}" --pos "${spos[*]}")
+  local pex_=("${exp[@]}" "$tfile" --checkbox-class "my-checkbox-class")
+  
+  assert_exit_code "0" "$("${ex_[@]}")";
+  assert_is_file "$tfile";
+
+  local pexout="$("${pex_[@]}")";
+  assert_exit_code "0" "$?";
+  assert_contains "my-checkbox-class" "$pexout";
+
+  rm "$tfile";
+}
+
+function test_html_no_form() {
+  local tfile="${tftmp_file}_html_no_form";
+  local ex_=("${ex[@]}" --output "$tfile" --opt "${sopt[*]}" --flag "${sflg[*]}" --nested "${snst[*]}" --pos "${spos[*]}")
+  local pex_=("${exp[@]}" "$tfile" --no-form)
+  
+  assert_exit_code "0" "$("${ex_[@]}")";
+  assert_is_file "$tfile";
+
+  local pexout="$("${pex_[@]}")";
+  assert_exit_code "0" "$?";
+  assert_not_contains "<form" "$pexout";
+
+  rm "$tfile";
+}
+
+function test_html_no_button() {
+  local tfile="${tftmp_file}_html_no_button";
+  local ex_=("${ex[@]}" --output "$tfile" --opt "${sopt[*]}" --flag "${sflg[*]}" --nested "${snst[*]}" --pos "${spos[*]}")
+  local pex_=("${exp[@]}" "$tfile" --no-button)
+  
+  assert_exit_code "0" "$("${ex_[@]}")";
+  assert_is_file "$tfile";
+
+  local pexout="$("${pex_[@]}")";
+  assert_exit_code "0" "$?";
+  assert_not_contains "<button" "$pexout";
+
+  rm "$tfile";
+}
+
+function test_html_no_js() {
+  local tfile="${tftmp_file}_html_no_js";
+  local ex_=("${ex[@]}" --output "$tfile" --opt "${sopt[*]}" --flag "${sflg[*]}" --nested "${snst[*]}" --pos "${spos[*]}")
+  local pex_=("${exp[@]}" "$tfile" --no-js)
+  
+  assert_exit_code "0" "$("${ex_[@]}")";
+  assert_is_file "$tfile";
+
+  local pexout="$("${pex_[@]}")";
+  assert_exit_code "0" "$?";
+  assert_not_contains "<script" "$pexout";
 
   rm "$tfile";
 }
